18b57200078731192a4f931fbde753668890a000
Lyndon Panton <LyndonPanton@gmail.com>
2025-09-15 17:25:19 +0100

    Feature: Created iterative fibonacci sequence method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Webpack successfully configured");
+// console.log("Webpack configured successfully");
+
+function fibs(n) {
+    let result = [];
+
+    let a = 0;
+    let b = 0;
+    let c = 1;
+
+    for (let i = 0; i < n; i++) {
+        result.push(a);
+        
+        a = c;
+        c = a + b;
+        b = a;
+    }
+    
+    return result;
+}
+
+console.log(fibs(8));
