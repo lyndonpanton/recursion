fe836c7bcf2ac5d28182521f2d31730688d96c07
Lyndon Panton <LyndonPanton@gmail.com>
2025-09-15 17:45:24 +0100

    Feature: Created recursive fibonacci sequence method

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function fibs(n) {
 
     for (let i = 0; i < n; i++) {
         result.push(a);
-        
+
         a = c;
         c = a + b;
         b = a;
@@ -18,4 +18,25 @@ function fibs(n) {
     return result;
 }
 
+
+function fibsRec(n, result = []) {
+    if (result.length < n) {
+        if (result.length == 0) {
+            result.push(0);
+        } else if (result.length == 1) {
+            result.push(1);
+        } else {
+            result.push(
+                result[result.length - 2]
+                + result[result.length - 1]
+            );
+        }
+
+        return fibsRec(n, result);
+    } else {
+        return result;
+    }
+}
+
 console.log(fibs(8));
+console.log(fibsRec(8));
