930beae3250ab3ad0e54f66cc22565d883078003
Lyndon Panton <LyndonPanton@gmail.com>
2025-09-19 18:28:35 +0100

    Feature: Merge sort algorithm can be called and displayed on the frontend

--- src/UI.js
+++ src/UI.js
@@ -5,11 +5,12 @@ class UI {
     constructor(main) {
         this.main = main;
         this.fibonacciCount = 0;
+        this.mergeSortList = [];
     }
 
     displayUI() {
         this.fibonacciUI();
-        // this.mergeSortUI();
+        this.mergeSortUI();
     }
 
     fibonacciCall(output, e) {
@@ -18,6 +19,9 @@ class UI {
         const result = fibsRec(this.fibonacciCount);
 
         output.textContent = result.join(", ");
+
+        // also clear input field
+        // fibonacciCount = 0
     }
 
     fibonacciUI() {
@@ -34,12 +38,12 @@ class UI {
         form.classList.add("container-form");
 
         let fibonacciLegend = document.createElement("legend");
-        fibonacciLegend.classList.add("fibonacci-form-legend");
+        fibonacciLegend.classList.add("form-legend");
         fibonacciLegend.textContent =
                 "How many numbers in the sequence would you like?";
 
         let fibonacciAmount = document.createElement("input");
-        fibonacciAmount.classList.add("fibonacci-form-count");
+        fibonacciAmount.classList.add("form-count");
         fibonacciAmount.max = 50;
         fibonacciAmount.min = 1;
         fibonacciAmount.required = true;
@@ -50,12 +54,18 @@ class UI {
         fibonacciAmount.addEventListener("keyup",
                 this.updateFibonacciCount.bind(this));
 
+        let formButton = document.createElement("input");
+        formButton.classList.add("form-submit");
+        formButton.value = "Generate";
+        formButton.type = "submit";
+
         form.appendChild(fibonacciLegend);
         form.appendChild(fibonacciAmount);
+        form.appendChild(formButton);
 
         const output = document.createElement("p");
         output.textContent = "...";
-        output.classList.add("fibonacci-output");
+        output.classList.add("form-output");
 
         form.addEventListener("submit", this.fibonacciCall.bind(this, output));
 
@@ -66,23 +76,87 @@ class UI {
         this.main.appendChild(container);
     }
 
+    mergeSortCall(output, e) {
+        e.preventDefault();
+
+        for (let i = 0; i < this.mergeSortList.length; i++) {
+            if (isNaN(this.mergeSortList[i])) {
+                // display with error message and return if value is not a number
+            } else {
+                this.mergeSortList[i] = parseInt(this.mergeSortList[i]);
+            }
+        }
+
+        console.log(this.mergeSortList);
+
+        mergeSort(this.mergeSortList);
+
+        output.textContent = this.mergeSortList.join(", ")
+
+        // also clear input field
+        // this.mergeSortList = [];
+    }
+
     mergeSortUI() {
         const container = document.createElement("div");
         container.id = "merge-sort";
         container.classList.add("container");
 
+        const heading = document.createElement("h2");
+        heading.classList.add("container-heading");
+        heading.textContent = "Merge Sort";
+
         const form = document.createElement("form");
         form.id = "merge-sort-form";
         form.classList.add("container-form");
 
+        let mergeSortLegend = document.createElement("legend");
+        mergeSortLegend.classList.add("form-legend");
+        mergeSortLegend.textContent =
+                "Enter space-separated values to sort";
+
+        let mergeSortValues = document.createElement("input");
+        mergeSortValues.classList.add("form-count");
+        mergeSortValues.maxLength = 50;
+        mergeSortValues.minLength = 1;
+        // Only only integers and spaces
+        // Do not allow two consecutive spaces?
+        // mergeSortValues.pattern = "";
+        mergeSortValues.required = true;
+        mergeSortValues.type = "text";
+        mergeSortValues.addEventListener("keydown",
+                this.updateMergeSortList.bind(this));
+        mergeSortValues.addEventListener("keyup",
+                this.updateMergeSortList.bind(this));
+
+        let formButton = document.createElement("input");
+        formButton.classList.add("form-submit");
+        formButton.value = "Merge and Sort";
+        formButton.type = "submit";
+
+        form.appendChild(mergeSortLegend);
+        form.appendChild(mergeSortValues);
+        form.appendChild(formButton);
+
+        const output = document.createElement("p");
+        output.textContent = "...";
+        output.classList.add("form-output");
+
+        form.addEventListener("submit", this.mergeSortCall.bind(this, output));
 
         container.appendChild(heading);
         container.appendChild(form);
         container.appendChild(output);
+
+        this.main.appendChild(container);
     }
 
     updateFibonacciCount(e) {
         this.fibonacciCount = e.target.value;
+    }
+
+    updateMergeSortList(e) {
+        this.mergeSortList = e.target.value.split(" ");
     }
 }
 
